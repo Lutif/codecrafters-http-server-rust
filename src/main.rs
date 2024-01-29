21a4a17a7962bea2a14213569420f6d60f6a1633
lutif <lutif.mandhro@gmail.com>
2024-01-29 22:27:11 +0500

    fix

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,45 @@
 // Uncomment this block to pass the first stage
-use std::{ net::TcpListener, io::{Write, Read} };
+use std::{
+    io::{Read, Write},
+    net::TcpListener,
+    str::from_utf8,
+};
 const ALLOWED_URL: &str = "/";
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
-
     // Uncomment this block to pass the first stage
     //
     let listener = TcpListener::bind("127.0.0.1:4221").unwrap();
-
     for stream in listener.incoming() {
         match stream {
-            Ok(mut stream) => {
-                let mut buffer = String::new();
-                match stream.read_to_string(&mut buffer) {
-                    Ok(_) => {
-                        let first_line: &str = buffer.lines().next().unwrap();
-                        let str_vec:Vec<&str> = first_line.split(" ").collect();
-                        let url = str_vec[1];
-                        println!("url line: {:?}", url);
-                        match url {
-                            ALLOWED_URL => {
-                                println!("Allowed URL");
-                                let response = "HTTP/1.1 200 OK\r\n\r\n";
-                                match stream.write(response.as_bytes()) {
-                                    Ok(_) => println!("Response sent"),
-                                    Err(e) => println!("Failed sending response: {}", e),
-                                };
-                            },
-                            _ => {
-                                println!("Not Allowed URL");
-                                let response = "HTTP/1.1 404 NOT FOUND\r\n\r\n";
-                                match stream.write(response.as_bytes()) {
-                                    Ok(_) => println!("Response sent"),
-                                    Err(e) => println!("Failed sending response: {}", e),
-                                };
-                            }
-                            
-                        }
-                    },
-
-                    Err(e) => println!("Failed reading request: {}", e),
+            Ok(mut _stream) => {
+                let mut buf = [0; 1024];
+                _stream.read(&mut buf).unwrap();
+                let req = from_utf8(&mut buf).unwrap();
+                let Some(first) = req.lines().next() else {
+                    let response = "HTTP/1.1 200 OK\r\n\r\n";
+                    let _ = _stream.write(response.as_bytes());
+                    return;
                 };
-                stream.flush().unwrap();
+                println!("first {}", first);
+                let Some(path) = first.split_whitespace().nth(1) else {
+                    let response = "HTTP/1.1 400 Bad Request\r\n\r\n";
+                    let _ = _stream.write(response.as_bytes());
+                    return;
+                };
+                println!("path {}", path);
+                if path == "/" {
+                    let response = "HTTP/1.1 200 OK\r\n\r\n";
+                    let _ = _stream.write(response.as_bytes());
+                } else {
+                    let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                    let _ = _stream.write(response.as_bytes());
+                }
             }
             Err(e) => {
                 println!("error: {}", e);
             }
         }
     }
-
 }
