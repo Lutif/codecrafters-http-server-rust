71b41cf0614a9a930485bda4f8daa2c984ff15b8
lutif <lutif.mandhro@gmail.com>
2024-01-30 10:04:06 +0500

    add echo route

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
 // Uncomment this block to pass the first stage
 use std::{
     io::{Read, Write},
-    net::TcpListener,
-    str::from_utf8,
+    net::TcpListener, 
+    str::from_utf8
 };
-const ALLOWED_URL: &str = "/";
+const NEW_LINE:&str = "\r\n\r\n";
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
@@ -29,12 +29,23 @@ fn main() {
                     return;
                 };
                 println!("path {}", path);
-                if path == "/" {
-                    let response = "HTTP/1.1 200 OK\r\n\r\n";
-                    let _ = _stream.write(response.as_bytes());
-                } else {
-                    let response = "HTTP/1.1 404 Not Found\r\n\r\n";
-                    let _ = _stream.write(response.as_bytes());
+                //if path includes echo, return 200 OK
+                
+                match path {
+                    "/" => {
+                        let response = format!("HTTP/1.1 200 OK{}",NEW_LINE);
+                        let _ = _stream.write(response.as_bytes());
+                    }
+                    _ if path.starts_with("/echo") => {
+                        let message = path.split("/echo").nth(1).unwrap().replace("/", "");
+                        let response =format!("HTTP/1.1 200 OK{}Content-Type: text/plain{}{}{}",NEW_LINE,NEW_LINE,message,NEW_LINE);
+                        let _ = _stream.write(response.as_bytes());
+                    }
+                    _ => {
+                        let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                        let _ = _stream.write(response.as_bytes());
+                    }
+                    
                 }
             }
             Err(e) => {
