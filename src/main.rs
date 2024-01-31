fb17ccfcc1a4e94c0fb5b71ad5dba63e4e7f8396
lutif <lutif.mandhro@gmail.com>
2024-01-31 19:03:35 +0500

    save files

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 // Uncomment this block to pass the first stage
 use std::{
-    io::{Read, Write},
-    net::TcpListener, 
-    str::from_utf8,
-    collections::HashMap,
-    thread::spawn as thread_spawn,
-    env::{args as get_args, Args},
-    path::{Path, PathBuf},
+    collections::HashMap, env::{args as get_args, Args}, fs, io::{Read, Write}, net::TcpListener, path::Path, str::from_utf8, thread::spawn as thread_spawn,
+    fs::File,
 };
 const NEW_LINE:&str = "\r\n";
 fn parse_headers(req: &str) -> HashMap<&str, &str> {
@@ -35,12 +30,21 @@ fn main() {
                     let mut buf = [0; 1024];
                 _stream.read(&mut buf).unwrap();
                 let req = from_utf8(&mut buf).unwrap();
+                let mut parts = req.split("\r\n\r\n");
+                let _headers = parts.next().unwrap();
+                let body = parts.next().unwrap_or("");
+
                 let Some(first) = req.lines().next() else {
                     let response = "HTTP/1.1 200 OK\r\n\r\n";
                     let _ = _stream.write(response.as_bytes());
                     return;
                 };
                 println!("first {}", first);
+                let Some(method) = first.split_whitespace().next() else {
+                    let response = "HTTP/1.1 400 Bad Request\r\n\r\n";
+                    let _ = _stream.write(response.as_bytes());
+                    return;
+                };
                 let Some(path) = first.split_whitespace().nth(1) else {
                     let response = "HTTP/1.1 400 Bad Request\r\n\r\n";
                     let _ = _stream.write(response.as_bytes());
@@ -69,18 +73,41 @@ fn main() {
                         let mut args:Args = get_args();
                         let directory = Some(args.nth(2)).unwrap().unwrap();
                         let file_name = path.split("/files/").nth(1).unwrap();
-                        //create path to file using directory and file_name
                         let path_name = Path::new(directory.as_str()).join(file_name);
-                        //check if file exists
-                        if !path_name.exists() {
-                            let response = "HTTP/1.1 404 Not Found\r\n\r\n";
-                            let _ = _stream.write(response.as_bytes());
-                            return;
-                        }else {
-                            let file_contents = std::fs::read_to_string(path_name).unwrap();
-                            let response =format!("HTTP/1.1 200 OK{}Content-Type: application/octet-stream{}Content-Length: {}{}{}{}{}",NEW_LINE,NEW_LINE,file_contents.chars().count(),NEW_LINE,NEW_LINE,file_contents,NEW_LINE);
-                            let _ = _stream.write(response.as_bytes());
+
+                        match method {
+                            "GET" => {
+                                print!("Got get request");
+                                if !path_name.exists() {
+                                    let response = "HTTP/1.1 404 Not Found\r\n\r\n";
+                                    let _ = _stream.write(response.as_bytes());
+                                    return;
+                                }else {
+                                    let file_contents = std::fs::read_to_string(path_name).unwrap();
+                                    let response =format!("HTTP/1.1 200 OK{}Content-Type: application/octet-stream{}Content-Length: {}{}{}{}{}",NEW_LINE,NEW_LINE,file_contents.chars().count(),NEW_LINE,NEW_LINE,file_contents,NEW_LINE);
+                                    let _ = _stream.write(response.as_bytes());
+                                }
+                            },
+                            "POST" => {
+                                print!("Got post request");
+                                print!("path name {}",body);
+                                let mut file = File::create(path_name).unwrap();
+                                file.write_all( body.as_bytes()).unwrap();
+                                let response = format!("HTTP/1.1 201 OK{}{}",NEW_LINE,NEW_LINE);
+                                let _ = _stream.write(response.as_bytes());
+                                return ;
+                            },
+                            _ => {
+                                let response = "HTTP/1.1 400 Bad Request\r\n\r\n";
+                                let _ = _stream.write(response.as_bytes());
+                                return;
+                            }
+                            
                         }
+
+                        //create path to file using directory and file_name
+                        //check if file exists
+
                         print!("file name {}",file_name);
                         // let response =format!("HTTP/1.1 200 OK{}Content-Type: text/plain{}Content-Length: {}{}{}{}{}",NEW_LINE,NEW_LINE,message.chars().count(),NEW_LINE,NEW_LINE,message,NEW_LINE);
                         // let _ = _stream.write(response.as_bytes());
